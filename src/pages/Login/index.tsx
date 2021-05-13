bac95a72d7bf5b00b5834c4069128625cee9c680
Viviane Bresolin <vivianebresolin@gmail.com>
2021-05-13 20:41:39 -0300

    Added Login page

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,24 @@
+import { Footer } from '../../components/Footer';
+
+import '../../assets/styles/login.scss';
+
+export function Login() {
+    return (
+        <>
+            <main>
+                <div className="greeting">
+                    <h1>Bem-vindo(a) ao <strong>Mundo dos Dragões!</strong></h1>
+                    <h2>Faça o login e entre nesse mundo mágico</h2>
+                </div>
+
+                <div className="login-container">
+                    <p>Login</p>
+                    <input placeholder="E-mail"></input>
+                    <input placeholder="Senha"></input>
+                    <button>Entrar</button>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+}
