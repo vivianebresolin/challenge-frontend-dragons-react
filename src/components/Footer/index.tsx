3bc138f1f34f588b535fcbae38d83a9bdd28421a
Viviane Bresolin <vivianebresolin@gmail.com>
2021-05-13 20:42:07 -0300

    Added component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FaLinkedin } from 'react-icons/fa';
+
+import '../../assets/styles/footer.scss';
+
+export function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2021 - Viviane Bresolin</p>
+            <a href="https://www.linkedin.com/in/vivianebresolin/" target="_blank" rel="noreferrer">
+                <FaLinkedin />
+            </a>
+        </footer>
+    );
+}
