2ea70bbcc7930a249ac47deff7162a019aa80e15
Viviane Bresolin <vivianebresolin@gmail.com>
2021-05-15 20:06:28 -0300

    Added button to Home to add dragon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,23 @@
+import { Link } from 'react-router-dom';
 import '../../assets/styles/home.scss';
 import { DragonsList } from '../../components/DragonsList';
+import { Header } from '../../components/Header';
 
 export function Home() {
     return (
-        <main id="main-home">
-            <h1>Escolha seu dragão:</h1>
-
-            <DragonsList />
-        </main>
+        <>
+            <Header />
+            <main id="main-home">
+                <div id="title">
+                    <h1>Escolha seu dragão ou </h1>
+                    <Link to='/register-dragon' style={{ textDecoration: 'none', fontSize: '1.3rem' }}>
+                        <button type="button" className="addDragon">
+                            adicione um novo dragão
+                        </button>
+                    </Link>
+                </div>
+                <DragonsList />
+            </main>
+        </>
     );
 }
