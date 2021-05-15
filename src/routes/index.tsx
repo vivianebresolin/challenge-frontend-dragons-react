aa6a4633ba1791b3267ee1977bbf8771aeca7998
Viviane Bresolin <vivianebresolin@gmail.com>
2021-05-15 20:00:34 -0300

    Updated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { Home } from '../pages/Home';
 import { DragonDetails } from '../pages/DragonDetails';
+import { RegisterDragon } from '../pages/RegisterDragon';
 
 export default function Routes() {
     return (
@@ -10,6 +11,7 @@ export default function Routes() {
                 <Route path="/" exact component={Login} />
                 <Route path="/home" exact component={Home} />
                 <Route path="/dragon-details" exact component={DragonDetails} />
+                <Route path="/register-dragon" exact component={RegisterDragon} />
             </Switch>
         </BrowserRouter>
     );
